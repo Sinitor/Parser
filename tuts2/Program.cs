16ae8631a17b7e698bb07bebd2c2707e60c8570a
Sinitor <98061711+Sinitor@users.noreply.github.com>
2022-03-19 17:49:17 +0200

    Yep

---
+++ tuts2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tuts2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pars;
+            WebClient web = new WebClient();
+            web.Encoding = Encoding.UTF8;
+            string page = web.DownloadString("https://orakul.com/");
+            string name = "<span class=\"name\">(.*?)</span>";
+            string page1 = web.DownloadString("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=8");
+            string name1 = "</strong>(.*?)</p>";
+            foreach (Match match in Regex.Matches(page, name))
+            {
+                pars = match.Groups[1].Value;
+                Console.WriteLine(pars);
+            }
+            foreach (Match match in Regex.Matches(page1, name1))
+            {
+                pars = match.Groups[1].Value;
+                Console.WriteLine(pars);
+            }
+        }
+    }
+}
